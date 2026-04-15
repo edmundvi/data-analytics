7041504515a2091a29142380c1feb07b37ed7356
edmund viray <eviray@my.yearupunited.org>
2026-04-15 11:33:42 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 This purpose of the sakila database is probably to store information of a film store.alter
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+SELECT * FROM film; -- Retrieved 1000 records
