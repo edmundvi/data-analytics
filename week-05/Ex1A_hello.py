376d142597398f10751cd63adfbaafe0106313bd
edmundvi <edmundv6@hotmail.com>
2026-05-04 13:47:37 -0700

    add Week_5_notes and update Ex1_hello.py

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world" prints twice because the run button is running the entire code, including the first and second methods to print 'Hello World!'
+
+#Displaying dollars and cents
+
+dollars = 3
+cents = .50
+
+print (dollars + cents)
+
+# The print statement added the two variables together as the integer I set them to, finding the sum of them
+
+cents = cents + .25
+
+print (dollars + cents)
+
